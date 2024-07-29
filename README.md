f97dafa75a51e86fe3cb45f1e679c185930b7700
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2024-07-29 17:15:19 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # archcraft-hyprland-config
 .config files for archcraft hyprland
+
+![Archcraft](./Screenshot_2024-07-29-17-12-02_19364.png)
---
+++ theme/current.bash
@@ -0,0 +1,28 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2024 Aditya Shakya <adi1090x@gmail.com>
+#
+# Catppucin Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#000000'
+foreground='#fdf6e3'
+color0='#263640'
+color1='#ec7875'
+color2='#61c766'
+color3='#fdd835'
+color4='#42a5f5'
+color5='#ba68c8'
+color6='#4dd0e1'
+color7='#bfbaac'
+color8='#4a697d'
+color9='#fb8784'
+color10='#70d675'
+color11='#ffe744'
+color12='#51b4ff'
+color13='#c979d7'
+color14='#5cdff0'
+color15='#fdf6e3'
+
+# Wallpaper
+wallpaper="$HOME/.config/hypr/wallpapers/wallpaper_black.jpg"
