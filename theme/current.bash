82779640c57b62b8c659249469b4eb077a1b9664
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2026-01-16 13:55:04 +0300

    Update theme and color scheme across configuration files
    Replaces the existing "Catppuccin-Mocha" theme with "Yetkin-Material" and updates the color scheme across multiple configuration files to use a new palette. Changes include modifying foreground, background, and accent colors for consistency in terminal, UI elements, and various applications. Also updates the wallpaper and addresses Electron scaling issues by setting the "ELECTRON_OZONE_PLATFORM_HINT" environment variable.
    
    Improves visual consistency and aligns the appearance with the new theme and design preferences.

--- theme/current.bash
+++ theme/current.bash
@@ -6,29 +6,29 @@
 
 # Colors
 background='#000000'
-foreground='#CDD6F4'
-color0='#45475A'
-color1='#F38BA8'
-color2='#A6E3A1'
-color3='#F9E2AF'
-color4='#89B4FA'
-color5='#F5C2E7'
-color6='#94E2D5'
-color7='#BAC2DE'
-color8='#585B70'
-color9='#F38BA8'
-color10='#A6E3A1'
-color11='#F9E2AF'
-color12='#89B4FA'
-color13='#F5C2E7'
-color14='#94E2D5'
-color15='#A6ADC8'
+foreground='#eeffff'
+color0='#263238'
+color1='#f07178'
+color2='#c3e88d'
+color3='#ffcb6b'
+color4='#82aaff'
+color5='#c792ea'
+color6='#89ddff'
+color7='#eeffff'
+color8='#263238'
+color9='#f07178'
+color10='#c3e88d'
+color11='#ffcb6b'
+color12='#82aaff'
+color13='#c792ea'
+color14='#89ddff'
+color15='#ffffff'
 
 # Wallpaper
-wallpaper="$HOME/.config/hyprcat/wallpapers/wallpaper.png"
+wallpaper="$HOME/.config/hyprcat/wallpapers/fauxcode-wallpaper.png"
 
 # GTK Theme
-gtk_theme='Catppuccin-Mocha'
+gtk_theme='Yetkin-Material'
 gtk_icons='Luv-Folders-Dark'
 gtk_font='Noto Sans 9'
 cursor_theme='Sweet'
